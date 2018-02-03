288c9f69967f4f5153f4bfdfdf5a707dbef47c2c
Yago <jagogc@gmail.com>
2018-02-03 17:42:08 +0000

    More Styling

--- src/Main.jsx
+++ src/Main.jsx
@@ -18,5 +18,11 @@ export default Main;
 
 const MainWrapper = styled.main`
 	max-width: 450px;
-	margin: 0 auto;
+	margin: 10px auto;
+	background-color: rgba(255,255,255,0.3);
+	border-radius: 3px;
+	padding: 15px;
+	@media (max-width: 420px) {
+		margin: 10px;
+	}
 `
--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {injectGlobal} from 'styled-components'
+import {injectGlobal, keyframes} from 'styled-components'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+const animation = keyframes`
+	0%{background-position:0% 50%}
+	50%{background-position:100% 50%}
+	100%{background-position:0% 50%}
+`
 
 injectGlobal`
 	body {
 		margin: 0;
 		padding: 0;
 		font-family: sans-serif;
+		background: linear-gradient(270deg, #8e0fdc, #663399);
+		background-size: 400% 400%;
+		animation: ${animation} 30s ease infinite;
 	}
 `
